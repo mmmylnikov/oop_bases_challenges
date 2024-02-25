8bea2fbd6e5980282c7e17d9ab3524ee00503712
Max Mylnikov <mmmylnikov@yandex.ru>
2024-02-25 12:17:17 +0500

    solved level 2 cases a,b

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -4,15 +4,25 @@
 Задания:
     1. Создайте класс User и перенесите всю логику работы с пользователем туда.
 """
+import dataclasses
 
 
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
+@dataclasses.dataclass(kw_only=True, slots=True)
 class User:
-    pass  # код писать тут
+    user_id: int
+    username: str
+    name: str
+
+    def make_username_capitalized(self) -> str:
+        return self.username.capitalize()
+
+    def generate_short_user_description(self) -> str:
+        output = f'User with id {self.user_id} '
+        output += f'has {self.username} username and {self.name} name'
+        return output
+
+
+user_instance = User(user_id=1, username='mmmylnikov', name='Max')
+print(user_instance)
+print(user_instance.make_username_capitalized())
+print(user_instance.generate_short_user_description())
--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -6,9 +6,31 @@
     2. Добавьте ему метод should_be_banned, который проверяет, должен ли быть пользователь забанен.
        Пользователя стоит забанить, если его фамилия находится в SURNAMES_TO_BAN.
 """
+import dataclasses
+
 
 SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
+@dataclasses.dataclass(kw_only=True, slots=True)
 class User:
-    pass  # код писать тут
+    first_name: str
+    last_name: str
+    age: int
+    is_banned: bool = False
+
+    def __post_init__(self) -> None:
+        self.should_be_banned()
+
+    def should_be_banned(self) -> None:
+        if self.last_name not in SURNAMES_TO_BAN:
+            return None
+        self.is_banned = True
+
+
+user_instances = [
+    User(first_name='Max', last_name='Mylnikov', age=29),
+    User(first_name='John', last_name='Porter', age=10)
+]
+for user in user_instances:
+    print(user)
